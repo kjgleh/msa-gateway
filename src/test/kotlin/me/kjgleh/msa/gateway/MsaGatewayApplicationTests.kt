c44fa9b2b2af4409f2a208819e6086fdf6110ed6
jung geun kim <junggeunkim@jungs-MacBook-Pro.local>
2021-10-29 22:50:22 +0900

    initialize the project

---
+++ src/test/kotlin/me/kjgleh/msa/gateway/MsaGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kjgleh.msa.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

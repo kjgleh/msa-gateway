9bed8c6290026ce1db0a93269b8c477837170009
kjg.leh <kjg.leh@gmail.com>
2021-10-30 00:26:59 +0900

    add gateway routes

--- src/main/kotlin/me/kjgleh/msa/gateway/Application.kt
+++ src/main/kotlin/me/kjgleh/msa/gateway/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MsaGatewayApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<MsaGatewayApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/test/kotlin/me/kjgleh/msa/gateway/ApplicationTests.kt
+++ src/test/kotlin/me/kjgleh/msa/gateway/ApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MsaGatewayApplicationTests {
+class ApplicationTests {
 
     @Test
     fun contextLoads() {
